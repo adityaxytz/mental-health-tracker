2fc0ccfe13265d2b4fb3b78639c1ecbb9318d33b
adityaxytz <dharmaa979@gmail.com>
2024-09-11 14:57:45 +0700

    tutorial

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://aditya-dharma31-mentalhealthtracker.pbp.cs.ui.ac.id", "https://aditya-dharma31-mentalhealthtracker.pbp.cs.ui.ac.id"]
